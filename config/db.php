0d686816b88c3df8c7884d655006b204850f8494
hoa2ng <n.khanhhoang@gmail.com>
2015-01-18 00:13:28 +0700

    change config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2base',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=admin_yiibase',
+    'username' => 'admin_yiibase',
+    'password' => 'oXEm9oSDMg',
     'charset' => 'utf8',
 ];
