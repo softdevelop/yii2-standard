7e53f09d934b1539ac606d1a50b1407cad34563e
hoa2ng <n.khanhhoang@gmail.com>
2015-03-01 21:36:59 +0700

    db

---
+++ migrations/m150225_100005_csettings_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150225_100005_csettings_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150225_100005_csettings_table cannot be reverted.\n";
+
+        return false;
+    }
+}
